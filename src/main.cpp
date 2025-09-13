622e7d4282fa291149cc771133ae321b0ddf6bec
Candy <swastiktoprani@gmail.com>
2025-09-13 11:56:59 +0000

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+
+int main() {
+std::string taskName;
+
+std::cout<<"TODO>";
+std::getline(std::cin, taskName);
+
+    std::cout<<"The task todo is :"<<taskName<<std::endl;
+
+    return 0;
+
+
+}
